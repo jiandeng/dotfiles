fade7d3b6fc836edd1706c087a312080e6272ad6
jiandeng <jiandeng.develop@gmail.com>
2016-12-06 22:19:02 +0800

    Silly support for ubuntu

---
+++ ubuntu/install.sh
@@ -0,0 +1,4 @@
+echo 'Installing essential package for Ubuntu...'
+sudo apt-get install git vim zsh wget curl python-pip python-dev cmake build-essential
+chsh -s /usr/bin/zsh
+echo 'Please logout the system to activate ZSH'
