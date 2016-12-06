937a90790404006bb35a2b40f489d453dc3a08b9
jiandeng <jiandeng.develop@gmail.com>
2016-12-06 23:00:42 +0800

    Install fasd on ubuntu

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,7 @@
 echo 'Installing essential package for Ubuntu...'
+sudo add-apt-repository ppa:aacebedo/fasd
+sudo apt-get update
+sudo apt-get install fasd
 sudo apt-get install git vim zsh wget curl python-pip python-dev cmake build-essential
 chsh -s /usr/bin/zsh
 echo 'Please logout the system to activate ZSH'
