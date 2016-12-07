0a29ce1f6e220585877ced2f2b77717c8857d410
jiandeng <jiandeng.develop@gmail.com>
2016-12-07 23:14:34 +0800

    Install ggplot for python

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -2,6 +2,6 @@ echo 'Installing essential package for Ubuntu...'
 sudo add-apt-repository ppa:aacebedo/fasd
 sudo apt-get update
 sudo apt-get install fasd
-sudo apt-get install git vim zsh wget curl python-pip python-dev cmake build-essential tmux ruby
+sudo apt-get install git vim zsh wget curl python-pip python-dev python-tk cmake build-essential tmux ruby
 chsh -s /usr/bin/zsh
 echo 'Please logout the system to activate ZSH'
