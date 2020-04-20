4c1a28403e340e35c5d76e97543567defb450601
Jiandeng <jiandeng.develop@gmail.com>
2020-04-20 10:09:08 +0800

    Install global (gtags)

--- essential/install.sh
+++ essential/install.sh
@@ -9,13 +9,13 @@ if [ "$(uname -s)" == "Darwin" ]; then
     ln -s /usr/local/bin/pip3 /usr/local/bin/pip
   fi
 
-  brew install colordiff git vim ack tmux cmake protobuf protobuf-c
+  brew install colordiff git vim ack tmux cmake protobuf protobuf-c global
   chsh -s /bin/zsh
 else
   sudo add-apt-repository ppa:aacebedo/fasd
   sudo apt-get update
   sudo apt-get install fasd ack-grep
-  sudo apt-get install colordiff git vim zsh wget curl python-pip python-dev python-tk cmake build-essential tmux ruby libbluetooth-dev
+  sudo apt-get install colordiff git vim zsh wget curl python-pip python-dev python-tk cmake build-essential tmux ruby libbluetooth-dev global
   pip install pybluez
   chsh -s /usr/bin/zsh
 fi
